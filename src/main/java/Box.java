f760f7791f87315907e139c81caf557311d35a13
Sébastien Canonica <sebcano.biz@laposte.net>
2020-06-03 06:56:12 +0200

    Starting point

--- src/main/java/Box.java
+++ src/main/java/Box.java
@@ -1,56 +1,7 @@
-import java.util.Objects;
-import java.util.function.Function;
-
 public class Box<T> {
     private T _value;
 
     public Box(T value) {
         _value = value;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Box<?> box = (Box<?>) o;
-        return Objects.equals(_value, box._value);
-    }
-
-    @Override
-    public int hashCode() {
-        return _value != null ? _value.hashCode() : 0;
-    }
-
-    public <U> Box<U> fmap(Function<T,U> fn) {
-        return new Box<U>(fn.apply(_value));
-    }
-
-    public static <T> Box<T> pure(T value) {
-        return new Box<T>(value);
-    }
-
-    public <U,V> Box<V> app(Box<U> box) {
-        @SuppressWarnings("unchecked")
-        Function<U, V> fn = (Function<U, V>) _value;
-        return new Box<V>(fn.apply(box._value));
-    }
-
-    public <U> Box<U> bind(Function<T, Box<U>> aIncrementToBox) {
-        return aIncrementToBox.apply(_value);
-    }
-
-    public <U> Box<U> fmapMonad(Function<T,U> fn) {
-        return bind(v -> // bind gives us the raw value of this as input
-                pure(fn.apply(v)) // so we can apply the raw function and put the result back in the box
-        );
-    }
-
-    public <U,V> Box<V> appMonad(Box<U> box) {
-        return bind(f -> { // bind gives us the raw function in this as input
-            @SuppressWarnings("unchecked")
-            Function<U, V> fn = (Function<U, V>) f;
-            return box.bind(u -> // now we can ask bind to unpack the value
-                    pure(fn.apply(u))); // apply and put back in the box
-        });
     }
 }
--- src/test/java/Box_should.java
+++ src/test/java/Box_should.java
@@ -6,9 +6,13 @@ import java.util.function.Function;
 import static org.junit.Assert.assertEquals;
 
 public class Box_should {
-    // RULE: NEVER EVER IMPLEMENT AN ACCESSOR !!!
+    // PRIME DIRECTIVE: NEVER EVER IMPLEMENT AN ACCESSOR !!!
     // This is the *feature* of our box
     // But how can we still do useful things with it ?
+    // Rules:
+    //      1) make the uncommented tests pass
+    //      2) uncomment the next test
+    //      3) go to step 1
 
     @Test
     public void consider_2_boxed_int_with_same_value_to_be_equals() {
@@ -17,23 +21,23 @@ public class Box_should {
         assertEquals(a2, another2);
     }
 
-    @Test
+/*    @Test
     public void implement_functor_fmap() {
         Box<Double> aDouble = new Box<>(5.55);
 
         Box<Long> actual = aDouble.fmap(Math::round);
 
         assertEquals(actual, new Box<>((long) 6));
-    }
+    }*/
 
-    @Test
+/*    @Test
     public void implement_applicative_pure() {
         Box<Integer> actual = Box.pure(3);
 
         assertEquals(actual, new Box<>(3));
-    }
+    }*/
 
-    @Test
+/*    @Test
     public void implement_applicative_app() {
         Box<Function<Integer, Double>> aBoxedIncrement = Box.pure(x -> x+1.5);
         Box<Integer> a5 = Box.pure(5);
@@ -41,9 +45,9 @@ public class Box_should {
         Box<Double> actual = aBoxedIncrement.app(a5);
 
         assertEquals(actual, Box.pure(6.5));
-    }
+    }*/
 
-    static class MyMath {
+/*    static class MyMath {
         static Integer add(Integer a, Integer b) {
             return a + b;
         }
@@ -62,9 +66,9 @@ public class Box_should {
         Function<Integer, Function<Short, Double>> actual = Curry.fi(biFn);
 
         assertEquals(biFn.apply(1,(short)2), actual.apply(1).apply((short)2));
-    }
+    }*/
 
-    @Test
+/*    @Test
     public void use_fmap_and_app_with_binary_functions() {
         BiFunction<Integer, Integer, Integer> add = (x, y) -> x+y;
         Box<Integer> a2 = Box.pure(2);
@@ -73,9 +77,9 @@ public class Box_should {
         Box<Integer> actual = a2.fmap(Curry.fi(add)).app(a5);
 
         assertEquals(actual, Box.pure(7));
-    }
+    }*/
 
-    @Test
+/*    @Test
     public void implement_monad_bind() {
         Box<String> securePassword = Box.pure("p@ssw0rd"); // Safely hidden in a box
         Function<String, Box<Integer>> secureHashing =
@@ -84,9 +88,9 @@ public class Box_should {
         Box<Integer> actual = securePassword.bind(secureHashing);
 
         assertEquals(actual, Box.pure("p@ssw0rd".hashCode()));
-    }
+    }*/
 
-    @Test
+/*    @Test
     public void reimplement_fmap_WITHOUT_USING_THE_RAW_VALUE() {
         // Hint: use bind and pure
         Box<Double> aDouble = new Box<>(5.55);
@@ -94,9 +98,9 @@ public class Box_should {
         Box<Long> actual = aDouble.fmapMonad(Math::round);
 
         assertEquals(actual, new Box<>((long) 6));
-    }
+    }*/
 
-    @Test
+/*    @Test
     public void reimplement_app_WITHOUT_USING_THE_RAW_VALUE() {
         // Hint: see above + you'll still need an ugly Function<...> cast
         Box<Function<Integer, Double>> aBoxedIncrement = Box.pure(x -> x+1.5);
@@ -105,5 +109,5 @@ public class Box_should {
         Box<Double> actual = aBoxedIncrement.appMonad(a5);
 
         assertEquals(actual, Box.pure(6.5));
-    }
+    }*/
 }
